c9e0c6d173dc9797233a555cafdcf13350068a69
Alexander Zeier <alex@hearthsim.net>
2017-06-16 14:05:23 +0200

    Classification analysis pseudo code

--- classification_test.py
+++ classification_test.py
@@ -71,3 +71,70 @@ for player_class, archetypes in labeled_data.items():
 				print("\t(%s) %s" % (score, deck))
 
 
+### Classification analysis:
+
+NUM_RUNS = 30
+MIN_CARDS = 1
+MAX_CARDS = 30
+
+# output dict
+classifications = defaultdict(defaultdict(float))
+
+# take number of random cards from a deck
+def take_random_cards(deck, card_count):
+	cards = list(deck.cards)
+	for _ in range(card_count):
+		index = random.randint(0, len(cards) - 1)
+		card = cards[index]
+		cards.remove(card)
+		yield card
+
+# list of full decks with known player_class and archetype
+# [{ cards: {1, 2, 2, 3, 4, ...}, player_class: "DRUID", archetype: "Token druid" }, ...]
+classified_decks = []
+
+# cluster set generated from the list above
+cluster_set = {}
+
+# iterate over all known decks
+for deck in classified_decks:
+
+	# iterate over number of cards for classification
+	for card_count in range(MIN_CARDS, MAX_CARDS):
+
+		# do several runs to get good result
+		for _ in range (NUM_RUNS):
+
+			# take given number of random cards from the deck
+			# could also introduce completely random cards here
+			cards = list(take_random_cards(deck, card_count))
+
+			# classify the random cards
+			cluster, score = cluster_set.classify_deck(cards)
+
+			# adjust scores if classification was successful
+			if cluster.name == deck.archetype:
+				classifications[card_count]["count"] += 1
+				classifications[card_count]["score"] += score
+
+# calculate final scores
+for card_count, values in classifications.items():
+	# average score on successful classification
+	average_score = values["score"] / values["count"]
+
+	# percent of correctly classified edcks
+	percent_classified = values["count"] / len(classified_decks) * NUM_RUNS * (MAX_CARDS - MIN_CARDS)
+
+	print("%i cards: %f%% success, score: %f" % (card_count, 100.0 * percent_classified, average_score))
+
+
+
+# OUTPUT:
+# 3 cards: 5% success, score: 5.42
+# 4 cards: 17% success, score 6.01
+# 5 cards: 36% success, score 14.23
+# 6 cards: 42% success, score 17.55
+# ...
+
+
+
